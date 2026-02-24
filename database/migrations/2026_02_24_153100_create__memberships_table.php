bd91d5da729d6bfd7177f93d9fbd40b82ce87c1c
youssef boualy <youssefboua913@gmail.com>
2026-02-24 16:39:10 +0100

    add migrations of table

---
+++ database/migrations/2026_02_24_153100_create__memberships_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('status');
+            $table->string('token');
+            $table->double('sold');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('calocation_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memberships');
+    }
+};
